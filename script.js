eab30666d3b624dec0a889b6c984fcf49ec3252b
naveen-aiml <naveenbwalikar8@gmail.com>
2026-01-15 00:32:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-const layers=document.querySelectorAll(".layer");
+// Parallax effect
+window.addEventListener("mousemove", (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 20;
+    const y = (e.clientY / window.innerHeight - 0.5) * 20;
 
-window.addEventListener("scroll",()=>{
-  const s=window.scrollY;
-
-  layers[0].style.transform=`translateY(${s*0.15}px)`; // bg
-  layers[1].style.transform=`translateY(${s*0.25}px)`; // glow
-
-  layers[2].style.transform=`translateY(${1000 - s*1}px)`;  // hero
-  layers[3].style.transform=`translateY(${2000 - s*1}px)`;  // about
-  layers[4].style.transform=`translateY(${3000 - s*1}px)`;  // project
-  layers[5].style.transform=`translateY(${4000 - s*1}px)`;  // contact
+    document.querySelector(".hero").style.backgroundPosition = 
+        `${50 + x}% ${50 + y}%`;
 });
